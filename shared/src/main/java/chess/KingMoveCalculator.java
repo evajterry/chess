60fe79fa41b76d629f17c8dd0ab1898095db95c3
Eva Terry <evaterry@Evas-MacBook-Pro-2.local>
2025-01-24 08:44:55 -0700

    cleaned up kingMoveCalculator

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 public class KingMoveCalculator implements ChessPieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        Collection<ChessMove> validMoves = new ArrayList<>(); // does declaring a type in line 8 instantiate anything?
+        Collection<ChessMove> validMoves = new ArrayList<>();
 
         int[] rowOffsets = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] colOffsets = {-1, 0, 1, -1, 1, -1, 0, 1};
@@ -17,14 +17,13 @@ public class KingMoveCalculator implements ChessPieceMovesCalculator {
         for (int i = 0; i < rowOffsets.length; i++) {
 
             int newRow = position.getRow() + rowOffsets[i];
-            int newCol = position.getColumn() + colOffsets[i]; // this could be wrong
+            int newCol = position.getColumn() + colOffsets[i];
 
             if (isPositionValid(newRow, newCol)) {
                 ChessPosition newPosition = new ChessPosition(newRow, newCol);
                 ChessPiece targetPiece = board.getPiece(newPosition);
 
                 if (targetPiece == null || targetPiece.getTeamColor() != teamColor) { // the board for test 1
-                    // the board for test is not empty - I think that's tripping the test
                     validMoves.add(new ChessMove(position, newPosition, null));
                 }
             }
